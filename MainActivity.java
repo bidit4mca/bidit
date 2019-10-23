bd741d39349b0411db78ec1ad6aa390029525185
biditmca <55690255+biditmca@users.noreply.github.com>
2019-10-23 12:18:53 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,14 +1,74 @@
-package com.example.adminhomepage;
+package com.example.myapplica;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity implements OnClickListener
+{
+    EditText subject,message;
+    Button submit;
+    SQLiteDatabase db;
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        subject=(EditText) findViewById(R.id.subject);
+        message=(EditText)findViewById(R.id.message);
+
+        submit=(Button) findViewById(R.id.submit);
+
+
+        submit.setOnClickListener(this);
+
+
+        db=openOrCreateDatabase("UserDB", Context.MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS Feedback(subject VARCHAR,message VARCHAR);");
+    }
+    public void onClick(View view) {
+
+        if (view == submit) {
+
+            if (subject.getText().toString().trim().length() == 0 ||
+                    message.getText().toString().trim().length() == 0) {
+
+                showMessage("Error", "Please enter all values");
+                return;
+            }
+            db.execSQL("INSERT INTO Feedback VALUES('" + subject.getText() + "','" + message.getText() + "');");
+            showMessage("Success", "Added successful");
+            clearText();
+        }
+    }
+
+
+
+    public void showMessage(String title,String message)
+    {
+        Builder builder=new Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
+    public void clearText()
+    {
+        subject.setText("");
+        message.setText("");
+
     }
 }
