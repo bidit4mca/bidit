0ccb90dd10bd794f0c210b114b038e57ae6cb547
biditmca <55690255+biditmca@users.noreply.github.com>
2019-11-12 14:21:47 +0530

    Add files via upload

--- payment.java
+++ payment.java
@@ -7,42 +7,48 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.EditText;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
+import android.app.NotificationManager;
 import android.widget.Toast;
 
-public class payment extends AppCompatActivity implements View.OnClickListener {
-    EditText Amount;
-    Button insert;
-    SQLiteDatabase db;
+public class payment extends AppCompatActivity  {
+
+    Button button;
     CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
-        Amount = (EditText) findViewById(R.id.editText);
-        insert = (Button) findViewById(R.id.button9);
         checkBox=(CheckBox)findViewById(R.id.checkbox);
-        insert.setOnClickListener(this);
-        db = openOrCreateDatabase("payment", Context.MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS payment(Amount VARCHAR);");
+
+        button=(Button)findViewById(R.id.button9);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ClickMe();
+                {Toast.makeText(payment.this,"Payment Successfully", Toast.LENGTH_LONG).show();}
+            }
+
+
+
+        });
     }
 
-    public void onClick(View view) {
+    private void ClickMe() {
+        Bitmap icon1= BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher_round);
+        NotificationCompat.BigTextStyle bigText=new NotificationCompat.BigTextStyle();
+        bigText.bigText("  Congratulations  on your  remarkable  achievement!     Your  Bidding is Succesfull.     Thank You.");
+        bigText.setBigContentTitle(" BID-IT  Notification");
+        //build notification
+        NotificationCompat.Builder mBuilder=(NotificationCompat.Builder)new NotificationCompat.Builder(this).setSmallIcon(R.mipmap.ic_launcher_round).setContentTitle("BID-IT Notification").setLargeIcon(icon1).setStyle(bigText);
+        NotificationManager mNotificationManager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0,mBuilder.build());
+    }
+}
 
-
-        if (view == insert) {
-            if (Amount.getText().toString().trim().length() == 0)
-            {
-                Toast.makeText(payment.this,"please Enter Amount!",Toast.LENGTH_SHORT).show();
-            }
-            else
-            {
-                db.execSQL("INSERT INTO payment VALUES('"+ Amount.getText()+"');");
-
-                Toast.makeText(payment.this,"Payment Successfull!",Toast.LENGTH_SHORT).show();
-            }
-
-
-        }}}
 
